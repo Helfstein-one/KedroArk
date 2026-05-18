8ec2fd9e3a28a4d7af9cab18f74028b3b306122b
Mauricio Helfstein <mauriciohelfstein@MacBook-Air-de-Mauricio.local>
2026-05-17 22:58:29 -0300

    chore: Remove generated test directory again

--- test_glue_project/src/test_glue_project/__main__.py
+++
@@ -1,6 +0,0 @@
-"""test-glue-project file for ensuring the package is executable."""
-
-from kedro.framework.cli import main
-
-if __name__ == "__main__":
-    main()
--- test_glue_project/terraform/emr/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_prefix" {
-  type        = string
-  description = "Prefix for project resources"
-}
-
-variable "environment" {
-  type        = string
-  description = "Deployment environment (dev, prod, etc)"
-}
